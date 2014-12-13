e4ff31c06c69613697f3aabd50dcc4fec55dad47
Pat Haggerty <haggertypat@gmail.com>
2014-12-13 14:50:24 -0500

    user bundle installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new AppUserBundle\AppUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             // new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle()
         );
 
